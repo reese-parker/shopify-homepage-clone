b91bd727963426831dc3690b0ba90b3dd32a37ae
Reese Parker <parker.rhys@outlook.com>
2022-04-02 18:06:07 -0400

    Added sections to app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import Navbar from "./Navbar";
+import IntroSection from "./IntroSection";
+import BusinessOnlineSection from "./BusinessOnlineSection";
+import styles from "./styles/AppStyles.module.css"
 
 function App() {
-  return <div className="App">
-    <Navbar />    
-  </div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <main className={styles.mainContainer} >
+        <IntroSection />
+        <BusinessOnlineSection />
+      </main>
+    </div>
+  );
 }
 
 export default App;
